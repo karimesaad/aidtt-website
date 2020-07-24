80b859bd1e1b10038ccba2f4c60bf1dee8311c5e
Karime Saad <karime.saad@utexas.edu>
2020-07-24 15:08:49 -0500

    Updates

---
+++ precache-manifest.2152abc7dd16153b4bce60c59d4fb9bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd6eb6dffd0913fecd5f7a26fab333a",
+    "url": "/aidtt-website/index.html"
+  },
+  {
+    "revision": "c4b8a69bd3a8aa543c86",
+    "url": "/aidtt-website/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "d4d714de7e8b92ff48b5",
+    "url": "/aidtt-website/static/css/main.162e9998.chunk.css"
+  },
+  {
+    "revision": "c4b8a69bd3a8aa543c86",
+    "url": "/aidtt-website/static/js/2.62b7e8d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aidtt-website/static/js/2.62b7e8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d714de7e8b92ff48b5",
+    "url": "/aidtt-website/static/js/main.36b91c63.chunk.js"
+  },
+  {
+    "revision": "210e4f61dca93758dd06",
+    "url": "/aidtt-website/static/js/runtime-main.5029bce7.js"
+  },
+  {
+    "revision": "d4611b4ef577e91c61786a76ad289e27",
+    "url": "/aidtt-website/static/media/arrow-down.d4611b4e.svg"
+  }
+]);
